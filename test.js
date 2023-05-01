d6fba042e01b528ae94e73da50ea8de4b0bd5d2a
Jacob-Moore_lmig <Jacob.Moore@LibertyMutual.com>
2023-05-01 13:53:24 -0400

    updated answers for the test today.

---
+++ test.js
@@ -0,0 +1,21 @@
+//Updated answers had I taken the time to write the code in vs vs notepad
+
+function multiply(a,b){
+   return a*b
+}
+console.log(multiply);
+
+const numbers = [1, 2, 3, 4, 5]
+let sum = 0
+function total(){
+for(let i=0; i<numbers.length;i++){
+   return sum += numbers[i];
+}};
+console.log(sum);
+
+
+const words =["One","Two","Three","Four","Five"]
+let add_exclamation = words.map(function(add) {
+  return add +"!"
+});
+console.log(add_exclamation);
