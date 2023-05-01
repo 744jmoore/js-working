b756e9bdd9c9937e19a0c06faa4b85ce93fb5f7b
Jacob-Moore_lmig <Jacob.Moore@LibertyMutual.com>
2023-05-01 12:07:25 -0400

    arrow_functions

---
+++ arrow_functions.js
@@ -0,0 +1,34 @@
+//Reducing functions to use the arrow function
+
+
+function sum(a,b){
+    return a+b
+document.querySelector('#total').innerHTML = sum
+}
+
+let sum2 = (a,b) => a+b 
+
+
+function isPositive(Number){
+    return Number >=0
+}
+
+let isPositive2 = (Number) => Number >=0
+
+function randomNumber(){
+    return Math.random
+}
+
+let randomNumber2 =() => Math.random
+
+document.addEventListener('click', function(){
+    console.log('Click')
+})
+
+document.addEventListener('click', ()=> console.log('click'))
+
+/// add content to the page
+const body = document.body
+const div =document.createElement('div')
+div.textContent="Adding some content"
+
